c855888410e0b20823985816ffb15b092e0185e4
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2022-09-26 19:55:35 +0100

    Initial Commit

---
+++ Nationalize App/Nationalize_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nationalize_AppApp.swift
+//  Nationalize App
+//
+//  Created by Bouchedoub Rmazi on 26/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct Nationalize_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
